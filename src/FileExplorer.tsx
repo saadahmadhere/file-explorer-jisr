a2e68d55115cffb18ee8925a3e1899688f5ec7a1
saadahmadhere <saadahmad728@gmail.com>
2024-10-01 19:23:20 +0530

    [feat]: add toggle functionality to the folder

--- src/FileExplorer.tsx
+++ src/FileExplorer.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FileData } from './types';
 import { Files } from './fileData';
 
 const FileExplorer: React.FC = () => {
+	const [expandedFolders, setExpandedFolders] = useState<string[]>([]);
+
+	const toggleFolder = (folderName: string) => {
+		if (expandedFolders.includes(folderName)) {
+			setExpandedFolders(expandedFolders.filter((name) => name !== folderName));
+		} else {
+			setExpandedFolders([...expandedFolders, folderName]);
+		}
+	};
 	const renderFiles = (data: FileData[]) => {
 		return data.map((item) => (
-			<div>{item.type === 'folder' ? `📂${item.name}` : `📄${item.name}`}</div>
+			<div>
+				{item.type === 'folder' ? (
+					<>
+						<div onClick={() => toggleFolder(item.name)}>
+							{expandedFolders.includes(item.name) ? '📂' : '📁'} {item.name}
+						</div>
+						{expandedFolders.includes(item.name) &&
+							item.data &&
+							renderFiles(item.data)}
+					</>
+				) : (
+					`📄${item.name}`
+				)}
+			</div>
 		));
 	};
 
